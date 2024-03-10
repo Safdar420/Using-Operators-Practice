34752e049bfe0cf21986610aa0c513777b3fe7e3
Safdar420 <160203243+Safdar420@users.noreply.github.com>
2024-03-09 23:08:00 -0800

    Add files via upload
    in this repository I create my project of typescript using  operators

---
+++ app2.js
@@ -0,0 +1,328 @@
+/////////// =================(1) ARTIMATHIC OPPERATORS IN TS ==========================///////////////////////
+// (1) addition operator -------------------------------->
+var a_3 = 5;
+var b_3 = 5;
+var sum = a_3 + b_3;
+console.log(sum);
+
+var x2 = 22;
+var y2 = 34;
+console.log(x2 + y2);
+
+var c_3 = 12;
+var d_3 = 12;
+console.log(c_3 + d_3);
+
+// (2) subtraction operator ------------------------>
+
+var a_4 = 12;
+var b_4 = 3;
+var sub1 = a_4 - b_4;
+console.log(sub1);
+
+var c_4 = 111;
+var d_4 = 11;
+var sub2 = c_4 - d_4;
+console.log(sub2);
+
+// (3) multiplication ------------------------->
+
+var a_5 = 4;
+var b_5 = 2;
+var multiply = a_5 * b_5;
+console.log(multiply);
+
+var c_5 = 5;
+var d_5 = 3;
+var multiply1 = c_5 * d_5;
+console.log(multiply1);
+
+// (4) division ---------------------------->
+
+var a_2 = 40;
+var b_2 = 2;
+var div = a_2 / b_2;
+console.log(div);
+
+var c_2 = 39;
+var d_2 = 3;
+var div1 = c_2 / d_2;
+console.log(div1);
+
+// (5) Modolous  ---------------------------> 
+
+var a_1 = (27 + 10);
+var b_1 = (2 + 8);
+var mod = a_1 % b_1;
+console.log(mod);
+
+var c_1 = (67 + 3);
+var d_1 = (3 + 8);
+var mod1 = c_1 % d_1;
+console.log(mod1);
+
+var e_1 = (47 + 2);
+var f_1 = (4 + 1);
+var mod2 = e_1 % f_1;
+console.log(mod2);
+
+var g_1 = (77 + 3);
+var h_1 = (5 + 2);
+var mod3 = g_1 % h_1;
+console.log(mod3);
+
+// // Power --------------------------------------------------------->
+
+var a = 2;
+var b = 3;
+var pow = Math.pow(a, b);
+console.log(pow);
+
+var c = 3;
+var d = 2;
+var pow1 = Math.pow(c, d);
+console.log(pow1);
+
+var e = 4;
+var f = 2;
+var pow2 = Math.pow(e, f);
+console.log(pow2);
+
+var g = 5;
+var h = 3;
+var pow3 = Math.pow(g, h);
+console.log(pow3);
+
+var i = 6;
+var j = 2;
+var pow4 = Math.pow(i, j);
+console.log(pow4);
+
+var k = 6;
+var l = 3;
+var pow5 = Math.pow(k, l);
+console.log(pow5);
+
+var m = 8;
+var n = 3;
+var pow6 = Math.pow(m, n);
+console.log(pow6);
+
+var o = 7;
+var p = 2;
+var pow7 = Math.pow(o, p);
+console.log(pow7);
+
+var q = 7;
+var r = 3;
+var pow8 = Math.pow(q, r);
+console.log(pow8);
+
+var s = 8;
+var t = 2;
+var pow9 = Math.pow(s, t);
+console.log(pow9);
+
+// ////////////////===================(2) ASSIGNMENT OPERATORS======================///////////////////////
+// (1) addition assignment ( += ) ----------------->
+
+var a1 = 5;
+a1 += 3;
+console.log(a1);
+
+var a2 = 6;
+a2 += 1;
+console.log(a2);
+
+// (2) subtraction assignment ( -= ) --------------->
+
+var a3 = 7;
+a3 -= 1;
+console.log(a3);
+
+var a4 = 9;
+a4 -= 5;
+console.log(a4);
+
+// (3) multiplication assignment ( *= ) --------->
+
+var a5 = 4;
+a5 *= 2;
+console.log(a5);
+
+var a6 = 5;
+a6 *= 2;
+console.log(a6);
+
+// (4) division assignment ( /= ) ----------->
+
+var a7 = 16;
+a7 /= 2;
+console.log(a7);
+
+var a8 = 16;
+a8 /= 4;
+console.log(a8);
+
+// (5) modolous assignment  ( %= ) --------->
+
+var a9 = 16;
+a9 %= 3;
+console.log(a9);
+
+var a10 = 16;
+a10 %= 5;
+console.log(a10);
+
+// (6) Power assignment ( **= )--------->
+
+var a11 = 3;
+a11 = Math.pow(a11, 2);
+console.log(a11);
+
+var a12 = 5;
+a12 = Math.pow(a12, 3);
+console.log(a12);
+
+///////////////////===============(3) COMPARISION OPERATOR =========================////////////////////////////
+// (1) { == or === } ----------------------------->
+
+var x = 21;
+var y = 5;
+console.log(x == y);
+
+var x1 = 5;
+var y1 = 5;
+console.log(x1 === y1);
+
+// (2)  Not equal to ( != ) ---------------------------->
+
+var num_1 = 5;
+var num_2 = 3;
+console.log(num_1 != num_2);
+
+var num_3 = 5;
+var num_4 = 5;
+console.log(num_3 != num_4);
+
+var num_5 = 15;
+var num_6 = 3;
+console.log(num_5 != num_6);
+
+var num_7 = 5;
+var num_8 = 13;
+console.log(num_7 != num_8);
+
+var num_9 = 5;
+var num_10 = 3;
+console.log(num_9 != num_10);
+
+var num_11 = 15;
+var num_12 = 13;
+console.log(num_11 != num_12);
+
+// (3) Greater than or less than ( > <)
+
+var c1 = 77;
+var c2 = 33;
+console.log(c1 > c2);
+
+var c3 = 77;
+var c4 = 33;
+console.log(c3 < c4);
+
+var c5 = 7;
+var c6 = 3;
+console.log(c5 > c6);
+
+var c7 = 7;
+var c8 = 3;
+console.log(c7 < c8);
+
+var c9 = 11;
+var c10 = 15;
+console.log(c9 > c10);
+
+var c11 = 11;
+var c12 = 15;
+console.log(c11 < c12);
+
+var c13 = 22;
+var c14 = 23;
+console.log(c13 > c14);
+
+var c15 = 22;
+var c16 = 23;
+console.log(c15 < c16);
+
+var c17 = 67;
+var c18 = 76;
+console.log(c17 > c18);
+
+var c19 = 67;
+var c20 = 76;
+console.log(c19 < c20);
+
+var c21 = 97;
+var c22 = 79;
+console.log(c21 > c22);
+
+var c23 = 97;
+var c24 = 79;
+console.log(c23 < c24);
+
+/// (4) Greater than equal to or less than equal to ( >= <= )--------------------->
+
+var num1 = 5;
+var num2 = 3;
+console.log(num1 >= num2);
+
+var num3 = 6;
+var num4 = 7;
+console.log(num3 >= num4);
+
+var num5 = 7;
+var num6 = 6;
+console.log(num5 >= num6);
+
+var num7 = 8;
+var num8 = 9;
+console.log(num7 <= num8);
+
+var num9 = 9;
+var num10 = 8;
+console.log(num9 >= num10);
+
+var num11 = 15;
+var num12 = 13;
+console.log(num11 >= num12);
+
+var num13 = 51;
+var num14 = 35;
+console.log(num13 <= num14);
+
+var num15 = 56;
+var num16 = 67;
+console.log(num15 <= num16);
+
+var num17 = 18;
+var num18 = 18;
+console.log(num17 <= num18);
+
+var num19 = 51;
+var num20 = 352;
+console.log(num19 >= num20);
+
+///////// (3)--------- Logical operators ----------------//////////////////
+// (1)  { && operator }------------>
+// in && operator if both conditions are true the in will be true
+// otherwise the result is falseght
+
+var g1 = 8;
+var g2 = 9;
+console.log(g1 > g2 && g2 > g1);
+
+var ab = 5;
+var cd = 7;
+var islessthan = (ab < cd && cd > ab);
+console.log(islessthan);
---
+++ app2.ts
@@ -0,0 +1,330 @@
+/////////// =================(1) ARTIMATHIC OPPERATORS IN TS ==========================///////////////////////
+// (1) addition operator -------------------------------->
+
+let a_3 : number = 5; 
+let b_3 : number = 5;
+let sum = a_3 + b_3;
+console.log(sum);
+
+let x2 : number = 22;
+let y2 : number = 34;
+console.log(x2 + y2);
+
+let c_3 : number = 12;
+let d_3 : number = 12;
+console.log(c_3 + d_3);
+
+// (2) subtraction operator ------------------------>
+
+let a_4 : number = 12;
+let b_4 : number = 3;
+let sub1 = a_4 - b_4;
+console.log(sub1);
+
+let c_4 : number = 111;
+let d_4 : number = 11;
+let sub2 = c_4 - d_4;
+console.log(sub2);
+
+// (3) multiplication ------------------------->
+
+let a_5 : number = 4;
+let b_5 : number = 2;
+let multiply = a_5 * b_5;
+console.log(multiply);
+
+let c_5 : number = 5;
+let d_5 : number = 3;
+let multiply1 = c_5 * d_5;
+console.log(multiply1);
+
+// (4) division ---------------------------->
+
+let a_2 : number = 40;
+let b_2 : number = 2;
+let div = a_2 / b_2;
+console.log(div);
+let  c_2 : number = 39;
+let d_2 : number = 3;
+let div1 = c_2 / d_2;
+console.log(div1);
+
+
+// (5) Modolous  ---------------------------> 
+
+let a_1 : number = (27 + 10);
+let b_1 : number = (2 +8);
+let mod = a_1 % b_1;
+console.log(mod);
+
+let c_1 : number = (67 + 3);
+let d_1 : number = (3 + 8);
+let mod1 = c_1 % d_1;
+console.log(mod1);
+
+let e_1 : number = (47 + 2);
+let f_1 : number = (4 + 1);
+let mod2 = e_1 % f_1;
+console.log(mod2);
+
+let g_1 : number = (77 + 3);
+let h_1 : number = (5 + 2);
+let mod3 = g_1 % h_1;
+console.log(mod3);
+
+// // Power --------------------------------------------------------->
+
+let a : number = 2;
+let b : number = 3;
+let pow = a ** b;
+console.log(pow);
+
+let c : number = 3;
+let d : number = 2;
+let pow1 = c ** d;
+console.log(pow1);
+
+let e : number = 4;
+let f : number = 2;
+let pow2 = e ** f;
+console.log(pow2);
+
+let g : number = 5;
+let h : number = 3;
+let pow3 = g ** h;
+console.log(pow3);
+
+let i : number = 6;
+let j : number = 2;
+let pow4 = i ** j;
+console.log(pow4);
+
+let k : number = 6;
+let l : number = 3;
+let pow5 = k ** l;
+console.log(pow5);
+
+let m : number = 8;
+let n : number = 3;
+let pow6 = m ** n;
+console.log(pow6);
+
+let o : number = 7;
+let p : number = 2;
+let pow7 = o ** p;
+console.log(pow7);
+
+let q : number = 7;
+let r : number = 3;
+let pow8 = q ** r;
+console.log(pow8);
+
+let s : number = 8;
+let t : number = 2;
+let pow9 = s ** t;
+console.log(pow9);
+
+// ////////////////===================(2) ASSIGNMENT OPERATORS======================///////////////////////
+
+// (1) addition assignment ( += ) ----------------->
+
+let a1 : number = 5; 
+a1 += 3;
+console.log(a1);
+
+let a2 : number = 6;
+a2 += 1;
+console.log(a2);
+
+// (2) subtraction assignment ( -= ) --------------->
+
+let a3 : number = 7;
+a3 -= 1;
+console.log(a3);
+
+let a4 : number = 9;
+a4 -= 5;
+console.log(a4);
+
+// (3) multiplication assignment ( *= ) --------->
+
+let a5 : number = 4;
+a5 *= 2; 
+console.log(a5);
+
+let a6 : number = 5;
+a6 *= 2; 
+console.log(a6);
+
+// (4) division assignment ( /= ) ----------->
+
+let a7 : number = 16;
+a7 /= 2; 
+console.log(a7);
+
+let a8 : number = 16;
+a8 /= 4; 
+console.log(a8);
+
+// (5) modolous assignment  ( %= ) --------->
+
+let a9 : number = 16;
+a9 %= 3; 
+console.log(a9);
+
+let a10 : number = 16;
+a10 %= 5; 
+console.log(a10);
+
+// (6) Power assignment ( **= )--------->
+
+let a11 : number = 3;
+a11 **= 2; 
+console.log(a11);
+
+let a12 : number = 5;
+a12 **= 3; 
+console.log(a12);
+
+///////////////////===============(3) COMPARISION OPERATOR =========================////////////////////////////
+
+// (1) { == or === } ----------------------------->
+
+let x = 21;
+let y = 5;
+console.log( x==y);
+
+let x1 = 5;
+let y1 = 5;
+console.log( x1===y1);
+
+// (2)  Not equal to ( != ) ---------------------------->
+
+let num_1 : number = 5;
+let num_2 : number = 3;
+console.log(num_1 != num_2);
+
+let num_3 : number = 5;
+let num_4 : number = 5;
+console.log(num_3 != num_4);
+
+let num_5 : number = 15;
+let num_6 : number = 3;
+console.log(num_5 != num_6);
+
+let num_7 : number = 5;
+let num_8 : number = 13;
+console.log(num_7 != num_8);
+
+let num_9 : number = 5;
+let num_10 : number = 3;
+console.log(num_9 != num_10);
+
+let num_11 : number = 15;
+let num_12 : number = 13;
+console.log(num_11 != num_12);
+
+// (3) Greater than or less than ( > <)
+
+let c1 : number = 77;
+let c2 : number = 33;
+console.log(c1 > c2);
+
+let c3 : number = 77;
+let c4 : number = 33;
+console.log(c3 < c4);
+
+let c5 : number = 7;
+let c6 : number = 3;
+console.log(c5 > c6);
+
+let c7 : number = 7;
+let c8 : number = 3;
+console.log(c7 < c8);
+
+let c9 : number = 11;
+let c10 : number = 15;
+console.log(c9 > c10);
+
+let c11 : number = 11;
+let c12 : number = 15;
+console.log(c11 < c12);
+
+let c13 : number = 22;
+let c14 : number = 23;
+console.log(c13 > c14);
+
+let c15 : number = 22;
+let c16 : number = 23;
+console.log(c15 < c16);
+
+let c17 : number = 67;
+let c18 : number = 76;
+console.log(c17 > c18);
+
+let c19 : number = 67;
+let c20 : number = 76;
+console.log(c19 < c20);
+
+let c21 : number = 97; 
+let c22 : number = 79;
+console.log(c21 > c22);
+
+let c23 : number = 97;
+let c24 : number = 79;
+console.log(c23 < c24);
+
+/// (4) Greater than equal to or less than equal to ( >= <= )--------------------->
+
+let num1 : number = 5;
+let num2 : number = 3;
+console.log(num1 >= num2);
+
+let num3 : number = 6;
+let num4 : number = 7;
+console.log(num3 >= num4);
+
+let num5 : number = 7;
+let num6 : number = 6;
+console.log(num5 >= num6);
+
+let num7 : number = 8;
+let num8 : number = 9;
+console.log(num7 <= num8);
+
+let num9 : number = 9;
+let num10 : number = 8;
+console.log(num9 >= num10);
+
+let num11 : number = 15;
+let num12 : number = 13;
+console.log(num11 >= num12);
+
+let num13 : number = 51;
+let num14 : number = 35;
+console.log(num13 <= num14);
+
+let num15 : number = 56;
+let num16 : number = 67;
+console.log(num15 <= num16);
+
+let num17 : number = 18;
+let num18 : number = 18;
+console.log(num17 <= num18);
+
+let num19 : number = 51;
+let num20 : number = 352;
+console.log(num19 >= num20);
+
+///////// (3)--------- Logical operators ----------------//////////////////
+// (1)  { && operator }------------>
+// in && operator if both conditions are true the in will be true
+// otherwise the result is falseght
+let g1 : number = 8;
+let g2 : number = 9;
+console.log(g1 > g2 && g2 > g1);
+
+let ab : number = 5;
+let  cd: number = 7;
+let islessthan = ( ab < cd && cd > ab );
+console.log(islessthan); 
